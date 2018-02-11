72d221b68d7e90c370cdfa496eaa7c52d85e6956
Killadave15 <davemeister_5@msn.com>
2018-02-11 12:39:45 -0800

    Added shell file for a Restroom Object

---
+++ app/src/main/java/doodoo/doodooordoodont/Restroom.java
@@ -0,0 +1,13 @@
+package doodoo.doodooordoodont;
+
+/**
+ * Created by David on 2/11/2018.
+ */
+
+public class Restroom {
+    private String UID;
+    private String name;
+    private float avgRating;
+    private int numRatings;
+    private int numStalls;
+}
