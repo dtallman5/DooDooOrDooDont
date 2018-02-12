e09d7ef081314566ca6bb00e3a2af34f8f572b0d
Killadave15 <davemeister_5@msn.com>
2018-02-12 14:18:27 -0800

    Added comments and method headers to MainActivity.java

--- app/src/main/java/doodoo/doodooordoodont/MainActivity.java
+++ app/src/main/java/doodoo/doodooordoodont/MainActivity.java
@@ -1,5 +1,4 @@
 package doodoo.doodooordoodont;
-//Test
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -27,13 +26,25 @@ public class MainActivity extends AppCompatActivity
 
     private GoogleMap mMap;
 
+    /**
+     * onCreate:
+     *
+     * This method is called every time the activity is created. It initializes all of the UI
+     * and starts the Google Map.
+     *
+     * @param savedInstanceState Holds data from previously saved states if it is not the first
+     *                           time the activity has been created. Null of first creation.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Sets the content view and initializes the toolbar
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //Creates the Floating button (currently non-functional)
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,23 +54,33 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
+        //Initializes the drawer layout and its toggle
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
+        //Initializes the view at the top of the drawer
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        //Initializes the google map fragment
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
+    /**
+     * onBackPressed:
+     *
+     * Called when the back button is pressed. If the drawer is open it closes it.
+     */
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        //Checks whether the drawer is open and closes it if it is
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -67,6 +88,15 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
+    /**
+     * onCreateOptionsMenu:
+     *
+     * This method is called when the options menu is created.
+     *
+     * @param menu The menu that is being created
+     *
+     * @return A boolean based on whether the menu was successfully created.
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -89,15 +119,25 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * onNavigationItemSelected:
+     *
+     * This method is called when an item in the navigation drawer is selected. It then initiates
+     * the appropriate response based on which item was selected.
+     *
+     * @param item The item that was selected and initiated the method call.
+     * @return A boolean based on whether the method executed successfully
+     */
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+        // Gets the id of the item selected
         int id = item.getItemId();
 
+        //Checks which item was selected and handles the appropriate action
         if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+
+         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
 
@@ -109,11 +149,20 @@ public class MainActivity extends AppCompatActivity
 
         }
 
+        //Closes the drawer
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
 
+    /**
+     * onMapReady:
+     *
+     * Called when the google map fragment is ready. Used to intialize markers and other factors
+     * associated with the map fragment
+     *
+     * @param googleMap The map that is ready
+     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
