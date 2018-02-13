14b0fbcc272329dd4e02fe05f4137a19dee6b969
Killadave15 <davemeister_5@msn.com>
2018-02-13 12:47:42 -0800

    Added constructor and getter/setter methods to Restroom.java

--- app/src/main/java/doodoo/doodooordoodont/Restroom.java
+++ app/src/main/java/doodoo/doodooordoodont/Restroom.java
@@ -7,7 +7,27 @@ package doodoo.doodooordoodont;
 public class Restroom {
     private String UID;
     private String name;
+
     private float avgRating;
     private int numRatings;
     private int numStalls;
+
+    public Restroom(String UID, String name) {
+        this.UID = UID;
+        this.name = name;
+    }
+
+
+    public void setRatings(float avgRating, int numRatings) {
+        this.numRatings = numRatings;
+        this.avgRating = avgRating;
+    }
+
+    public float getAvgRating() {
+        return avgRating;
+    }
+
+    public int getNumRatings() {
+        return numRatings;
+    }
 }
