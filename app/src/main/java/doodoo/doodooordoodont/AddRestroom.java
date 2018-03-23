68476cb4435e7352806b36a958a621b12a4d8b35
Killadave15 <davemeister_5@msn.com>
2018-03-23 15:49:46 -0700

    Further updated the AddRestroom page and temporarily made the Restroom object Parcelable in order to test the add Restroom feature.

--- app/src/main/java/doodoo/doodooordoodont/AddRestroom.java
+++ app/src/main/java/doodoo/doodooordoodont/AddRestroom.java
@@ -1,12 +1,21 @@
 package doodoo.doodooordoodont;
 
+import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RadioGroup;
+import android.widget.RatingBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -15,17 +24,57 @@ import android.widget.TextView;
  * Created by David on 3/11/2018.
  */
 
-public class AddRestroom extends AppCompatActivity {
+public class AddRestroom extends AppCompatActivity implements TextWatcher{
+
+    private Button send;
+    private RadioGroup gender;
+    private EditText name;
+    private RatingBar ratings;
+    double lon, lat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Intent in = this.getIntent();
+
+        lon = in.getDoubleExtra("Lon",0);
+        lat = in.getDoubleExtra("Lat", 0);
+
 
         //Sets the content view and initializes the toolbar
         setContentView(R.layout.content_add_restroom);
         Toolbar toolbar = (Toolbar) findViewById(R.id.addtoolbar);
         toolbar.setTitle("Add Restroom");
         setSupportActionBar(toolbar);
+
+        ratings = (RatingBar) findViewById(R.id.ratingBar);
+
+        send = (Button) findViewById(R.id.sendButton);
+        send.setClickable(false);
+        send.setTextColor(getResources().getColor(R.color.colorLightGray));
+        send.setAlpha(0.5f);
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createRestroom();
+            }
+        });
+
+
+        TextView coord = (TextView) findViewById(R.id.coordinateText);
+        coord.setText("Coordinates:  " + Double.toString(lat) + ", " + Double.toString(lon));
+
+        name = (EditText) findViewById(R.id.nameField);
+        name.addTextChangedListener(this);
+
+        gender = (RadioGroup) findViewById(R.id.genderGroup);
+        gender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                checkFields();
+            }
+        });
 
         Spinner stallSpinner = (Spinner) findViewById(R.id.stallSpinner);
         String[] stallitems = {"0","1","2","3","4","5","6","7+"};
@@ -53,6 +102,17 @@ public class AddRestroom extends AppCompatActivity {
 
         this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         this.getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close_white_24dp);
+
+    }
+
+    private void createRestroom() {
+        Restroom toAdd = new Restroom("003", name.getText().toString());
+        toAdd.setRatings(ratings.getRating(),1,ratings.getRating(),1);
+        toAdd.setLocation(lat, lon);
+        MainActivity.addMarker(toAdd);
+        Intent backHome = new Intent(this,MainActivity.class);
+        backHome.putExtra("Restroom", toAdd);
+        startActivity(backHome);
     }
 
 
@@ -69,4 +129,25 @@ public class AddRestroom extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void checkFields(){
+        if (!name.getText().toString().equals("") && gender.getCheckedRadioButtonId() != -1){
+            send.setTextColor(getResources().getColor(R.color.colorWhite));
+            send.setAlpha(1f);
+            send.setClickable(true);
+        }
+        else{
+            send.setTextColor(getResources().getColor(R.color.colorLightGray));
+            send.setAlpha(0.5f);
+            send.setClickable(false);
+        }
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+    @Override
+    public void afterTextChanged(Editable editable) {
+        checkFields();
+    }
 }
--- app/src/main/java/doodoo/doodooordoodont/Restroom.java
+++ app/src/main/java/doodoo/doodooordoodont/Restroom.java
@@ -1,10 +1,13 @@
 package doodoo.doodooordoodont;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by David on 2/11/2018.
  */
 
-public class Restroom {
+public class Restroom implements Parcelable{
     private String UID;
     private String name;
 
@@ -14,6 +17,7 @@ public class Restroom {
     private float fAvgRating;
     private int fNumRatings;
     private int fNumStalls;
+    private double lat,lon;
 
     public static boolean menDisplayed;
 
@@ -62,4 +66,62 @@ public class Restroom {
     public static void setMenDisplayed(boolean bool) {
         menDisplayed = bool;
     }
+
+    public void setLocation(double lat, double lon) {
+        this.lat = lat;
+        this.lon = lon;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    protected Restroom(Parcel in) {
+        UID = in.readString();
+        name = in.readString();
+        mAvgRating = in.readFloat();
+        mNumRatings = in.readInt();
+        mNumStalls = in.readInt();
+        fAvgRating = in.readFloat();
+        fNumRatings = in.readInt();
+        fNumStalls = in.readInt();
+        lat = in.readDouble();
+        lon = in.readDouble();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(UID);
+        dest.writeString(name);
+        dest.writeFloat(mAvgRating);
+        dest.writeInt(mNumRatings);
+        dest.writeInt(mNumStalls);
+        dest.writeFloat(fAvgRating);
+        dest.writeInt(fNumRatings);
+        dest.writeInt(fNumStalls);
+        dest.writeDouble(lat);
+        dest.writeDouble(lon);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<Restroom> CREATOR = new Parcelable.Creator<Restroom>() {
+        @Override
+        public Restroom createFromParcel(Parcel in) {
+            return new Restroom(in);
+        }
+
+        @Override
+        public Restroom[] newArray(int size) {
+            return new Restroom[size];
+        }
+    };
 }
